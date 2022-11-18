525b2a84788b9715740500c171a832e0cf3406ed
Larsa-mrp <bradleyspiclin@gmail.com>
2022-11-18 19:27:32 +1100

    Added default properties for Models and Enum for game genre

---
+++ Data/GameGenre.cs
@@ -0,0 +1,14 @@
+﻿namespace eGameStore.Data
+{
+    public enum GameGenre
+    {
+        RoleplayingGame = 1,
+        Strategy,
+        Simulation,
+        Sandbox,
+        Puzzle,
+        ActionAdventure,
+        Shooter
+
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using eGameStore.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace eGameStore.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public GameGenre GameGenre { get; set; }
+
+    }
+}
