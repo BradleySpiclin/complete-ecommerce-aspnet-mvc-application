56400be33bcfb9de00fbb717425d3ea27ffb9bdf
Larsa-mrp <bradleyspiclin@gmail.com>
2023-01-23 13:53:16 +1100

    Updated Publishers controller - all CRUD implemeneted and working for Publisher Model

--- Controllers/PublishersController.cs
+++ Controllers/PublishersController.cs
@@ -17,13 +17,14 @@ namespace eGameStore.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allPublishers= await _service.GetAllAsync();
+            var allPublishers = await _service.GetAllAsync();
             return View(allPublishers);
         }
 		public IActionResult Create()
 		{
 			return View();
 		}
+
 		// POST: Publishers/Create/
 		[HttpPost]
 		public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Publisher publisher)
@@ -32,6 +33,58 @@ namespace eGameStore.Controllers
 				return View(publisher);
 
 			await _service.AddAsync(publisher);
+			return RedirectToAction(nameof(Index));
+		}
+
+		// Get request
+		public async Task<IActionResult> Details(int id)
+		{
+			var publisherDetails = await _service.GetByIdAsync(id);
+			if (publisherDetails == null)
+				return View("NotFound");
+
+			return View(publisherDetails);
+		}
+
+
+		// GET: Publishers/Edit/1
+		public async Task<IActionResult> Edit(int id)
+		{
+			var publisherDetails = await _service.GetByIdAsync(id);
+			if (publisherDetails == null)
+				return View("NotFound");
+
+			return View(publisherDetails);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Publisher publisher)
+		{
+			if (!ModelState.IsValid)
+				return View(publisher);
+
+			await _service.UpdateAsync(id, publisher);
+			return RedirectToAction(nameof(Index));
+		}
+
+		// GET: Publishers/Delete/1
+		public async Task<IActionResult> Delete(int id)
+		{
+			var publisherDetails = await _service.GetByIdAsync(id);
+			if (publisherDetails == null)
+				return View("NotFound");
+
+			return View(publisherDetails);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var publisherDetails = await _service.GetByIdAsync(id);
+			if (publisherDetails == null)
+				return View("NotFound");
+
+			await _service.DeleteAsync(id);
 			return RedirectToAction(nameof(Index));
 		}
 	}
--- Models/Publisher.cs
+++ Models/Publisher.cs
@@ -3,19 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace eGameStore.Models
 {
-    public class Publisher : IEntityBase
+    public class Publisher : IEntityBase //inherits from the base interface
     {
-        [Key]
-        public int Id { get; set; }
-        
-        [Display(Name = "Publisher Logo")]
-        public string ProfilePictureURL { get; set; }
+		[Key]
+		public int Id { get; set; }
+		[Display(Name = "Publisher Logo")]
+		[Required(ErrorMessage = "Profile picture required")]
+		public string ProfilePictureURL { get; set; }
 		[Display(Name = "Publisher Name")]
+		[Required(ErrorMessage = "Publisher name required")]
+		[StringLength(50, MinimumLength = 3, ErrorMessage = "Full name must be between 3 - 50 characters")]
 		public string FullName { get; set; }
+
 		[Display(Name = "Biography")]
+		[Required(ErrorMessage = "Biography required")]
 		public string Bio { get; set; }
 
-        //Relationships
-        public List<Game> Games { get; set; }
-    }
+		//Relationships
+		public List<Game>? Games { get; set; }
+	}
 }
